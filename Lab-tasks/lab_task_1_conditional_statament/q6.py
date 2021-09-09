459daa4f7acac7a279d0fc6a2251e67be6faa545
Waqar <74553567+daniell-waqar@users.noreply.github.com>
2021-09-09 22:15:43 +0500

    Delete q6.py

--- Lab-tasks/lab_task_1_conditional_statament/q6.py
+++
@@ -1,9 +0,0 @@
-x = int(input("enter size: "))
-y = int(input("enter size: "))
-z = int(input("enter size: "))
-if x == y == z:
-    print("equilateral triangle")
-elif x == y != z or x != y == z or x == z != y:
-    print("isoceles triangle")
-else:
-    print("scalene trianle")
