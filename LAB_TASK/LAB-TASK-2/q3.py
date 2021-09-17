a09004b2c57fb445589e436c0776b7a0598cd473
daniell-waqar <p200750@pwr.nu.edu.pk>
2021-09-17 23:45:01 +0500

    program to check whether a given number is even or odd.

---
+++ LAB_TASK/LAB-TASK-2/q3.py
@@ -0,0 +1,7 @@
+
+number = int(input()) # getting input from user
+if number % 2 == 0:   # taking mod of input with 2 
+    print("even") # printing it  is even
+else:                # otherwise
+    print("odd")   #print it is odd
+    
