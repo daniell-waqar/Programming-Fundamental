e6225e60dbb1d74e877d64ce576741693acd8208
daniell-waqar <p200750@pwr.nu.edu.pk>
2021-09-17 23:42:24 +0500

    read any digit between (0 - 9) from the user and display it in the word.

---
+++ LAB_TASK/LAB-TASK-2/q2.py
@@ -0,0 +1,25 @@
+
+n = int(input("enter a number b/w 0 to 9: ")) #input from user 
+if n == 0:        # if input value is zero
+    print("zero")      #print it
+elif n == 1:        # if input value is one
+    print("one")          #print it
+elif n == 2:   #    if input value is two
+    print("two")    #print it
+elif n == 3:          # if input value is three
+    print("three")  #print it
+elif n == 4:             # if input value is four
+    print("four")          #print it
+elif n == 5:               # if input value is five
+    print("five")             #print it
+elif n == 6:                     # if input value is six
+    print("six")                        #print it
+elif n == 7:                        # if input value is seven
+    print("seven")                     #print it
+elif n == 8:                          # if input value is eight
+    print("eight")                           #print it
+elif n == 9:                                    # if input value is nine
+    print("nine")                                      #print it
+else:                   # otherwise
+    print("invalid input")      # input is not valid
+
