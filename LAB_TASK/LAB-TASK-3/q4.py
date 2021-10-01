09b7bb0d267f3b56bf72bdddb9e69b3b3916eef9
daniell-waqar <p200750@pwr.nu.edu.pk>
2021-10-02 00:11:40 +0500

    function that takes quantity in pounds and convert it into gram and kilogram.

---
+++ LAB_TASK/LAB-TASK-3/q4.py
@@ -0,0 +1,7 @@
+def fun(pound):        # function defined
+    a = pound*0.454   #convert pound into kilogram
+    b = pound*453.59  #convert pound into gram
+    print(a,"kilograms") #printing value into kilogram
+    print(b,"grams")     #printing value into gram
+    
+fun(1)               #function call
