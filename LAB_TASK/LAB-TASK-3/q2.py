ef6c77f910b175fec181137c8947e26e4bc7b816
daniell-waqar <p200750@pwr.nu.edu.pk>
2021-10-02 00:07:19 +0500

    function that takes the length, width and height of a parallelepiped and finds its volume.

---
+++ LAB_TASK/LAB-TASK-3/q2.py
@@ -0,0 +1,4 @@
+def cal_volume(length,width,height):  #define function
+    volume = length*width*height  #calculating volume of parallelopied 
+    return volume    # return volume of parallelopied 
+cal_volume(2,3,4)    #function call
