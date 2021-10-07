2e5affd6089573f31f8d570533a93a57c364666e
daniell-waqar <p200750@pwr.nu.edu.pk>
2021-10-08 01:27:17 +0500

    program which will find all such numbers which are divisible by 7 but are not a multiple of 5,between 2000 and 3200 (both included).

---
+++ LAB_TASK/LAB-TASK-4/q2.py
@@ -0,0 +1,3 @@
+for i in range(2000,3200):  # iterate over range btw 2000 and 3200
+    if i % 7 == 0 and i % 5 != 0:    # if number divisible by 7 and not divisible by 5
+        print(i)    # print it
